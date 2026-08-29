fa4a2e0042e724c23e03a1799166d03f0400012e
Jakob Tormalm <jakob.tormalm@gmail.com>
2026-08-29 22:02:14 +0200

    feat(nav): add custom Requests (Jellyseerr) link to drawer navigation

--- src/scripts/libraryMenu.js
+++ src/scripts/libraryMenu.js
@@ -326,6 +326,7 @@ function refreshLibraryInfoInDrawer(user) {
     let html = '';
     html += '<div style="height:.5em;"></div>';
     html += `<a is="emby-linkbutton" class="navMenuOption lnkMediaFolder" href="#/home"><span class="material-icons navMenuOptionIcon home" aria-hidden="true"></span><span class="navMenuOptionText">${globalize.translate('Home')}</span></a>`;
+    html += `<a is="emby-linkbutton" class="navMenuOption lnkMediaFolder" href="https://mer.jellyfin.nu/" target="_blank" rel="noopener noreferrer"><span class="material-icons navMenuOptionIcon search" aria-hidden="true"></span><span class="navMenuOptionText">Requests</span></a>`;
 
     // placeholder for custom menu links
     html += '<div class="customMenuOptions"></div>';
