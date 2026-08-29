2bdd28fc4a0f8dced6c546271157996717863734
Jakob Tormalm <jakob.tormalm@gmail.com>
2026-08-29 21:12:48 +0200

    feat(ui): add custom CSS theme and bundle Google Sans & Material Icons fonts

--- src/index.jsx
+++ src/index.jsx
@@ -45,6 +45,7 @@ import './styles/livetv.scss';
 import './styles/dashboard.scss';
 import './styles/detailtable.scss';
 import './styles/librarybrowser.scss';
+import './assets/css/custom.css';
 
 async function init() {
     // Log current version to console to help out with issue triage and debugging
